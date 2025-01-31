5815206363c98a7473a83b39cefdc207b8860a8d
ASUS <dudeimperfect72@gmail.com>
2025-02-01 00:01:08 +0530

    operators

---
+++ Comparision.java
@@ -0,0 +1,11 @@
+public class Comparision {
+    public static void main(String[] args){
+        int x =783;
+        int y =767;
+        System.out.println(x==y);
+        System.out.println(x>y);
+        System.out.println(x<=y);
+        System.out.println(x>=y);
+        System.out.println(x<y);
+    }
+}
